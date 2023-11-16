7587d1dfeeb8342e2de06984449f83e25daee969
Bob Parsons <deadstylep@gmail.com>
2023-11-16 15:57:46 -0600

    Cleanup

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"time"
 
-	scheduler "github.com/bparsons094/go-server-base/scheduler/schedules"
 	"github.com/go-co-op/gocron"
 	"gorm.io/gorm"
 )
@@ -14,12 +13,5 @@ func InitScheduler(DB *gorm.DB) {
 
 	s := gocron.NewScheduler(time.UTC)
 
-	s.Every(1).Hour().Do(func() {
-		scheduler.CleanUserCache()
-	})
-
-	s.Every(1).Day().At("00:00").Do(func() {
-		scheduler.CleanRequestLogs(DB)
-	})
 	s.StartBlocking()
 }
--- scheduler/schedules/hourly.scheduler.go
+++
@@ -1,9 +0,0 @@
-package scheduler
-
-import (
-	"github.com/bparsons094/go-server-base/utils"
-)
-
-func CleanUserCache() {
-	utils.ClearExpiredUsers()
-}
